0a58797fb8b876a54f03f04e59d91048796ecf0d
Dima Maltsev <phoenixnety@gmail.com>
2022-07-11 14:58:51 +0300

    added pageobjects

---
+++ src/test/java/tests/SearchTests.java
@@ -0,0 +1,46 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import pages.MainPage;
+import pages.SearchPage;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTests {
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $(byName("q")).setValue("selenide").pressEnter();
+        $("#=search").shouldHave(text("https://ru.selenide.org"));
+    }
+
+    @Test
+    void successfulSearchWithNewPageObjectsTest() {
+        new MainPage().openPage();
+        new MainPage().typeSearch("selenide");
+        new SearchPage().checkResult("https://ru.selenide.org");
+    }
+
+
+    @Test
+    void successfulSearchWithPageObjectsTest() {
+        MainPage mainPage = new MainPage();
+        SearchPage searchPage = new SearchPage();
+
+        mainPage.openPage();
+        mainPage.typeSearch("selenide");
+        searchPage.checkResult("https://ru.selenide.org");
+    }
+
+    @Test
+    void successfulSearchWithFluentTest() {
+        MainPage mainPage = new MainPage();
+
+        mainPage.openPage()
+                .typeSearch("selenide")
+                .checkResult("https://ru.selenide.org");
+    }
+}
